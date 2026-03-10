87d678a4f0351891dc262fcd69a3d59cd9f97b53
Pcko <126863503+Pcko@users.noreply.github.com>
2026-03-11 00:09:28 +0100

    feature(auth): implement auth-middleware

--- crates/server/src/router.rs
+++ crates/server/src/router.rs
@@ -1,12 +1,20 @@
+use crate::middleware::auth_session::AuthSession;
 use crate::routes::{auth, health, user};
 use crate::state::AppState;
-use axum::Router;
+use axum::extract::FromRequestParts;
+use axum::middleware::FromExtractor;
+use axum::{Router, middleware};
 use tower_cookies::CookieManagerLayer;
 
 pub fn app(state: AppState) -> Router {
+    let users = user::router()
+        .route_layer(middleware::from_extractor_with_state::<AuthSession, _>(
+            state.clone(),
+        ));
+
     Router::new()
-        .nest("/users", user::router())
         .nest("/auth", auth::router())
+        .nest("/users", users)
         .merge(health::router())
         .layer(CookieManagerLayer::new())
         .with_state(state)
