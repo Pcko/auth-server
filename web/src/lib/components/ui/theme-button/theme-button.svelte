47baf13821e56fae267c970b5e177e4b985aa64d
Pcko <126863503+Pcko@users.noreply.github.com>
2026-04-24 17:45:32 +0200

    feat(web): add theme toggle button

---
+++ web/src/lib/components/ui/theme-button/theme-button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import {toggleMode,mode} from 'mode-watcher'
+    import SunIcon from '../../../assets/SunIcon.svelte'
+    import MoonIcon from '../../../assets/MoonIcon.svelte'
+</script>
+
+
+<button onclick={toggleMode}
+        class="fixed bottom-8 right-9 z-50 inline-flex h-15 w-15 items-center justify-center border bg-card">
+    {#if mode.current==="light"}
+        <SunIcon/>
+    {:else}
+        <MoonIcon/>
+    {/if}
+</button>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import './app.css';
-    import favicon from '$lib/assets/favicon.svg';
+    import favicon from '$lib/assets/favicon/favicon.svg';
     import {ModeWatcher} from "mode-watcher";
 
     let {children} = $props();
